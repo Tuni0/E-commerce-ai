86b662426975432b61b230b40564e9623153fd3e
Tuni0 <wiktor.mazepa@gmail.com>
2026-01-07 08:29:48 +0100

    cofniecie path

--- backend/server/index.js
+++ backend/server/index.js
@@ -799,10 +799,6 @@ app.post("/create-checkout-session", isAuthenticated, async (req, res) => {
   }
 });
 
-app.get("/", (req, res) => {
-  res.json({ status: "OK", service: "Backend API" });
-});
-
 // ===== START SERVERA =====
 app.listen(3006, () => {
   console.log("🚀 Server running on port 3006");
